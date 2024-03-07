1dda99d25ece449f74f860e1332eb77d50a62222
Medhat Elmasry <medhat@elmasry.ca>
2024-03-07 12:08:47 -0800

    1st

---
+++ students-react-ts/src/models/Student.ts
@@ -0,0 +1,9 @@
+export class Student {
+    constructor(
+        public StudentId: number, 
+        public FirstName: string, 
+        public LastName: string, 
+        public School: string) {
+    }
+  }
+  
